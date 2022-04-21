a00e23f882feca84e7b37103967a360b36cfb593
Max <max.mjm.melo@gmail.com>
2022-04-20 21:40:15 -0400

    Weight constraints added

--- ga/ksnapsack.py
+++ ga/ksnapsack.py
@@ -1,6 +1,7 @@
+#Based on:
+# https://machinelearningmastery.com/simple-genetic-algorithm-from-scratch-in-python/
 
-from audioop import reverse
-import enum
+
 from numpy.random import randint
 from numpy.random import rand
 from functools import reduce
@@ -41,7 +42,7 @@ def mutation(bitstring, r_mut):
         if rand() < r_mut:
             bitstring[i] = 1-bitstring[i]
 
-def bag_sum(cr):
+def cr_to_int(cr):
     s = []
     for i,pos in enumerate(range(0,n_bits, n_bits_cr)):
         gene = cr[pos:pos+n_bits_cr]
@@ -52,6 +53,11 @@ def bag_sum(cr):
         size = reduce(lambda sum, x: sum+ x[0]*2**x[1], zip(gene,exp), 0)
         s.append(size)
 
+    return s
+
+def bag_sum(cr):
+    s = cr_to_int(cr)
+
     s = np.array(s)
     
     return (sum(s*weights), sum(s*benefits))
@@ -60,19 +66,23 @@ def bag_sum(cr):
 
 def genetic_alg(fitness_fn, r_cross, r_mut):
     population = [randint(0, 2, n_bits).tolist() for _ in range(n_pop)]
-    print(population[0])
     score = fitness_fn(population[0])
-    print(score)
+    if score[0] > max_cap:
+        population[0] = list(np.zeros(20, dtype=int))
+        score = (0.,0.)
 
-    best_score = (0, score) # 0: índice, 10: peso, 11: benefício
+    #print(population[0])
+    #print(score)
+
+    best_score = (population[0].copy(), score) # 0: índice, 10: peso, 11: benefício
 
     for gen in range(max_iter):
         scores = [fitness_fn(cr) for cr in population]
 
         for i, score in enumerate(scores):
-            if score[1] > best_score[1][1]:
-                best_score = (i, score)
-                print('new best score found at ', i, 'score: ', best_score[1])
+            if score[0]< max_cap and score[1] > best_score[1][1]:
+                best_score = (population[i].copy(), score)
+                #print('new best score found at ', i, '-st element, score: ', best_score[1])
 
         selected = [selection(population, scores) for _ in range(n_pop)]
         #print(np.shape(selected))
@@ -86,9 +96,13 @@ def genetic_alg(fitness_fn, r_cross, r_mut):
                 children.append(c)
             
         population = children
+        if gen%5==0:
+            print(f'Generation[{gen}], Best found [Weight: {best_score[1][0]}, Benefit: {best_score[1][1]}]')
+
     return best_score
             
 
 
 solution = genetic_alg(bag_sum, 0.9, 1.0/float(n_bits))
-print('best found: ', solution)
+print(f'Best found [Weight: {solution[1][0]}, Benefit: {solution[1][1]}]')
+print(cr_to_int(solution[0]))
